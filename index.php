59e3da486fb2f95984cd2eae16ca37810e204e93
Jesus Antonio Lopez Bandala <lopezbandalajesusantonio@gmail.com>
2024-12-03 15:24:54 -0600

    .env y conexion a base de datos en Model.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-    require_once "_controller/CtrlPaginaPrincipal.php";
-    $ctrl = new CtrlPaginaPrincipal();
-    require_once "_controller/condomino/CtrlInvitados.php";
-    $ctrl = new CtrlInvitados();
-    include "_view/master.html";
-?>
+ini_set('display_errors', E_ALL); //Esta linea solo es para pruebas, no dejar en produccion
+require_once "_controller/CtrlPaginaPrincipal.php";
+$ctrl = new CtrlPaginaPrincipal();
+require_once "_controller/condomino/CtrlInvitados.php";
+$ctrl = new CtrlInvitados();
+include "_view/master.html";
