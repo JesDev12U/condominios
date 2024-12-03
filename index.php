6f08adab728f4b75fe1f945ec03b399c9d301ade
FishefM <yucefhernandez@hotmail.com>
2024-12-03 15:04:11 -0600

    Correxion de rutas y versiones de boostrap

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
     require_once "_controller/CtrlPaginaPrincipal.php";
     $ctrl = new CtrlPaginaPrincipal();
+    require_once "_controller/condomino/CtrlInvitados.php";
+    $ctrl = new CtrlInvitados();
     include "_view/master.html";
 ?>
