a1eb9cd897ef6bf42150ef0a2e69cbc17598fba7
Jesus Antonio Lopez Bandala <lopezbandalajesusantonio@gmail.com>
2024-12-26 02:29:27 -0600

    Correccion de errores con la lectura del INE

--- _controller/ajaxDatosINE.php
+++ _controller/ajaxDatosINE.php
@@ -17,61 +17,75 @@ try {
       $response['exec_output'] = $output;
       $response['exec_return_code'] = $returnCode;
 
-      if ($returnCode === 0) {
-        $response['status'] = 'success';
-        $response['fulltext'] = file_get_contents($outputFile . '.txt');
-        $patternName = '/(?<=NOMBRE\s)[A-Z\s]+(?=\sDOMICILIO)/i'; //regex para el nombre
-        $patternAddress = '/(?<=DOMICILIO\s)[A-Z\s0-9\.|\-_\)\(\*\&\^\%\$\#\@\!\/,]+(?=\sCLAVE)/i'; //regex para el domicilio
-        $patternCURP = '/\b([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)\b/';
-
-        //Validamos el nombre
-        if (preg_match($patternName, $response['fulltext'], $matches)) {
-          $splitName = [];
-          $name = trim($matches[0], "\n");
-          $cont = 0;
-          $arrayName = explode("\n", $name);
-          for ($i = 0; $i < count($arrayName); $i++) {
-            if ($arrayName[$i] === " " || $arrayName[$i] === null || $arrayName[$i] === "") continue;
-            switch ($cont) {
-              case 0: {
-                  $splitName['paternal_surname'] = $arrayName[$i];
-                }
-              case 1: {
-                  $splitName['maternal_surname'] = $arrayName[$i];
-                }
-              case 2: {
-                  $splitName['names'] = $arrayName[$i];
-                }
-            }
-            $cont++;
-          }
-          $response['name'] = $splitName;
-        } else {
-          $response['status'] = 'error';
-          $response['error'] = 'NOMBRE no encontrado';
-        }
-
-        //Validamos el domicilio
-        if (preg_match($patternAddress, $response['fulltext'], $matches)) {
-          $response['address'] = str_replace("\n", " ", trim($matches[0], "\n"));
-        } else {
-          $response['status'] = 'error';
-          $response['error'] = 'DOMICILIO no encontrado';
-        }
-
-        //Validamos el CURP
-        if (preg_match($patternCURP, $response['fulltext'], $matches)) {
-          $response['curp'] = $matches[0];
-        } else {
-          $response['status'] = 'error';
-          $response['error'] = "CURP no encontrada";
-        }
-        // Limpia el archivo temporal
-        unlink($outputFile . '.txt');
-      } else {
+      if ($returnCode !== 0) {
         $response['status'] = 'error';
         $response['error'] = 'Error al procesar la imagen con Tesseract.';
+        throw new Error();
       }
+
+      $response['status'] = 'success';
+      $response['fulltext_spa'] = file_get_contents($outputFile . '.txt');
+      exec("tesseract $tempFile $outputFile -l eng", $output, $returnCode);
+      // Depuración: Verifica si Tesseract se ejecutó
+      $response['exec_output'] = $output;
+      $response['exec_return_code'] = $returnCode;
+
+      if ($returnCode !== 0) {
+        $response['status'] = 'error';
+        $response['error'] = 'Error al procesar la imagen con Tesseract.';
+        throw new Error();
+      }
+
+      $response['status'] = 'success';
+      $response['fulltext_eng'] = file_get_contents($outputFile . '.txt');
+      $patternName = '/(?<=NOMBRE\s)[A-Z\s]+(?=\sDOMICILIO)/i'; //regex para el nombre
+      $patternAddress = '/(?<=DOMICILIO\s)[A-Z\s0-9\.|\-_\)\(\*\&\^\%\$\#\@\!\/,]+(?=\sCLAVE)/i'; //regex para el domicilio
+      $patternCURP = '/\b([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)\b/';
+
+      //Validamos el nombre
+      if (preg_match($patternName, $response['fulltext_spa'], $matches) || preg_match($patternName, $response['fulltext_eng'], $matches)) {
+        $splitName = [];
+        $name = trim($matches[0], "\n");
+        $cont = 0;
+        $arrayName = explode("\n", $name);
+        for ($i = 0; $i < count($arrayName); $i++) {
+          if ($arrayName[$i] === " " || $arrayName[$i] === null || $arrayName[$i] === "") continue;
+          switch ($cont) {
+            case 0: {
+                $splitName['paternal_surname'] = $arrayName[$i];
+              }
+            case 1: {
+                $splitName['maternal_surname'] = $arrayName[$i];
+              }
+            case 2: {
+                $splitName['names'] = $arrayName[$i];
+              }
+          }
+          $cont++;
+        }
+        $response['name'] = $splitName;
+      } else {
+        $response['status'] = 'error';
+        $response['error'] = 'NOMBRE no encontrado';
+      }
+
+      //Validamos el domicilio
+      if (preg_match($patternAddress, $response['fulltext_spa'], $matches) || preg_match($patternAddress, $response['fulltext_eng'], $matches)) {
+        $response['address'] = str_replace("\n", " ", trim($matches[0], "\n"));
+      } else {
+        $response['status'] = 'error';
+        $response['error'] = 'DOMICILIO no encontrado';
+      }
+
+      //Validamos el CURP
+      if (preg_match($patternCURP, $response['fulltext_spa'], $matches) || preg_match($patternCURP, $response['fulltext_eng'], $matches)) {
+        $response['curp'] = $matches[0];
+      } else {
+        $response['status'] = 'error';
+        $response['error'] = "CURP no encontrada";
+      }
+      // Limpia el archivo temporal
+      unlink($outputFile . '.txt');
     } else {
       $response['status'] = 'error';
       $response['error'] = 'Error al subir el archivo.';
